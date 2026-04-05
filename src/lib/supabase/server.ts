a38b8a1f8504ca33e2ca7cddecf91a00bcc572d0
Khaled Yosry <khaledyosry@Khaleds-MacBook-Air.local>
2026-04-05 22:48:19 +0400

    fix server client types

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,15 +2,25 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export async function createClient() {
-  const c = await cookies()
+  const cookieStore = await cookies()
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll: () => c.getAll(),
-        setAll: (list) =>
-          list.forEach(({ name, value, ...o }) => c.set(name, value, o)),
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // ignore
+          }
+        },
       },
     }
   )
